134ed85b2ea8ddb41d6a0e1b2dba7b6856474f6c
黃靖淳 <kiki@starktech.com.tw>
2013-11-19 11:04:41 +0800

    Initial Commit

---
+++ CoreImageTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CoreImageTest
+//
+//  Created by kiki Huang on 13/11/19.
+//  Copyright (c) 2013年 kiki Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
